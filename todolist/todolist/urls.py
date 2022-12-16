53d3634290a4ced093ded7e94f07e71841d74d30
Женя <mr.holistr@mail.ru>
2022-12-16 22:04:04 +0300

    Fix urls

--- todolist/todolist/urls.py
+++ todolist/todolist/urls.py
@@ -19,7 +19,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/core/', include('core.urls')),
+    path('core/', include('core.urls')),
     path('oauth/', include('social_django.urls', namespace='social')),
-    path('api/goals/', include('goals.urls')),
+    path('goals/', include('goals.urls')),
 ]
